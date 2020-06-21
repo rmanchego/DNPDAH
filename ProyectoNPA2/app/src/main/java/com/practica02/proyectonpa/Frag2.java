066e8554adf951c288cc0d45bfa56f8fe78a2cb8
rmanchegom <manchego3103@gmail.com>
2020-06-21 01:02:06 -0500

    Primera parte

---
+++ ProyectoNPA2/app/src/main/java/com/practica02/proyectonpa/Frag2.java
@@ -0,0 +1,42 @@
+package com.practica02.proyectonpa;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Frag2 extends Fragment {
+
+    private Button btnAgregarVisita;
+
+    @SuppressLint("ResourceType")
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag2_layout,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        btnAgregarVisita = (Button) getView().findViewById(R.id.buttonVisita);
+
+        btnAgregarVisita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AgregarVisita.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
