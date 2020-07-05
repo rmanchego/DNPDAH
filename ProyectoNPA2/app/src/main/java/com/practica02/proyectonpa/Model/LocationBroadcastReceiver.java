b3b1f5abef20c3baf01da6b2eabd60aa90efb1f3
AngelVM <angelvgmh@gmail.com>
2020-07-04 21:00:50 -0500

    Se agregó el código de Rastrear Ubicación con BroadcastReceiver, falta unirlo a su Fragment

---
+++ ProyectoNPA2/app/src/main/java/com/practica02/proyectonpa/Model/LocationBroadcastReceiver.java
@@ -0,0 +1,44 @@
+package com.practica02.proyectonpa.Model;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+
+import com.practica02.proyectonpa.Controller.UbicacionActivity;
+
+import java.security.Provider;
+
+public class LocationBroadcastReceiver extends BroadcastReceiver {
+
+    public double latitud,longitud;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.hasExtra(LocationManager.KEY_LOCATION_CHANGED)){ //Constante de cambio de ubicación
+            String locationKey = LocationManager.KEY_LOCATION_CHANGED;
+            Location ubicacion = (Location) intent.getExtras().get(locationKey);
+            latitud = ubicacion.getLatitude();
+            longitud = ubicacion.getLongitude();
+        }
+
+        else {
+            Toast.makeText(context,"Null",Toast.LENGTH_SHORT).show();
+        }
+        enviarDatos(context);
+    }
+    public void enviarDatos(Context context){
+        Intent intentActivity = new Intent(context, UbicacionActivity.class);
+        intentActivity.putExtra("LATITUDE", String.valueOf(latitud));
+        intentActivity.putExtra("LONGITUDE", String.valueOf(longitud));
+        intentActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intentActivity);
+
+    }
+}
