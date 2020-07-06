8caf81c5f999e4d0790bc01061397b782061f735
AngelVM <angelvgmh@gmail.com>
2020-07-06 03:29:18 -0500

    Ubicación con activity, pero marca latitud = 0 y longitud = 0. Arreglar eso

--- ProyectoNPA2/app/src/main/java/com/practica02/proyectonpa/Model/LocationBroadcastReceiver.java
+++ ProyectoNPA2/app/src/main/java/com/practica02/proyectonpa/Model/LocationBroadcastReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Bundle;
 
 public class LocationBroadcastReceiver extends BroadcastReceiver {
     private UbicacionActivityInf ubicacionActivityInf;
@@ -13,9 +14,13 @@ public class LocationBroadcastReceiver extends BroadcastReceiver {
     }
     @Override
     public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
         if (intent.hasExtra(LocationManager.KEY_LOCATION_CHANGED)) {
             String locationChanged = LocationManager.KEY_LOCATION_CHANGED;
             Location location = (Location) intent.getExtras().get(locationChanged);
+            if (bundle!=null){
+
+            }
             double latitude = location.getLatitude();
             double longitude = location.getLongitude();
             ubicacionActivityInf.DisplayLocationChange(latitude,longitude);
