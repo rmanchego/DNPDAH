a359931ac164590605a30d5927cc420ef95bef8d
Rodolfo Manchego <rmanchegom@unsa.edu.pe>
2020-07-30 20:43:16 -0500

    Agregando datos del contador de pasos a la base de datos de Firebase

---
+++ ProyectoNPA2/app/src/main/java/com/practica02/proyectonpa/Model/Entidades/Firebase/Pasos.java
@@ -0,0 +1,17 @@
+package com.practica02.proyectonpa.Model.Entidades.Firebase;
+
+public class Pasos {
+    private int pasos;
+
+    public Pasos(){
+
+    }
+
+    public int getPasos() {
+        return pasos;
+    }
+
+    public void setPasos(int pasos) {
+        this.pasos = pasos;
+    }
+}
