efa85a0f86c41578412415eedad66e09611e218e
AngelVM <angelvgmh@gmail.com>
2020-08-09 11:22:10 -0500

    Se agregaron los sensores Acelerometro y Giroscopio para su posterior uso en la cámara

---
+++ ProyectoNPA2/app/src/main/java/com/practica02/proyectonpa/Controller/Sensores/Acelerometro/Acelerometro.java
@@ -0,0 +1,45 @@
+package com.practica02.proyectonpa.Controller.Sensores.Acelerometro;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class Acelerometro {
+    private ListenerAcelerometro listenerAcelerometro;
+
+    public void setListenerAcelerometro(ListenerAcelerometro listenerAcelerometro) {
+        this.listenerAcelerometro = listenerAcelerometro;
+    }
+    private SensorManager sensorManager;
+    private Sensor sensorAcelerometro;
+    private SensorEventListener sensorEventListener;
+
+    public Acelerometro(Context context){
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        sensorAcelerometro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if(listenerAcelerometro!=null){
+                    listenerAcelerometro.ejes(event.values[0],event.values[1],event.values[2]);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+    }
+
+    public void register(){
+        sensorManager.registerListener(sensorEventListener,sensorAcelerometro,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void unregister(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+}
---
+++ ProyectoNPA2/app/src/main/java/com/practica02/proyectonpa/Controller/Sensores/Acelerometro/ListenerAcelerometro.java
@@ -0,0 +1,5 @@
+package com.practica02.proyectonpa.Controller.Sensores.Acelerometro;
+
+public interface ListenerAcelerometro {
+    void ejes(float ejex, float ejey, float ejez);
+}
