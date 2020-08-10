a24995d8a3a6038a4fb9512e41d3ccbb97b3b012
AngelVM <angelvgmh@gmail.com>
2020-08-10 15:19:08 -0500

    Se mejoraron estilos y se quitaron partes no usadas

--- ProyectoNPA2/app/src/main/java/com/practica02/proyectonpa/Controller/MainActivity.java
+++ ProyectoNPA2/app/src/main/java/com/practica02/proyectonpa/Controller/MainActivity.java
@@ -77,16 +77,8 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         toolbar = (Toolbar) findViewById(R.id.titletoolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
         setSupportActionBar(toolbar);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        System.out.print("Hola");
+
 
 
 
